22cafe281d49ced6f5ce1f02f03b5002e7b19b27
Elijah Y <74162307+Lij175@users.noreply.github.com>
2023-03-24 09:56:00 -0700

    eclipse

--- Code/src/PrinterWritingExtention.java
+++ Code/src/PrinterWritingExtention.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 // 50 < X < 220
 // 50 < Y < 220
 public class PrinterWritingExtention {
+	//test
 
 	public static String user = "ejyst";
 	public static void main(String[] args) throws FileNotFoundException {
